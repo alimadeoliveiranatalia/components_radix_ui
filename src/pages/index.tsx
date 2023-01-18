2aad0fbe87e1c9e531f976d97f17293ae33d924b
alimadeoliveiranatalia <alimadeoliveiranatalia@gmail.com>
2023-01-18 16:45:15 -0300

    feat: verify add radix component Modal

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../pages/Home.module.css'
 import { PopoverRadix } from '../components/PopoverRadix'
 import { ModalRadix } from '@/components/ModalRadix'
-import { ContentModal } from '@/components/ContentModal'
 
 
 export default function Home() {
   return (
     <div>
       <PopoverRadix />
+      <br />
+      <br />
       <ModalRadix />
     </div>
   )
